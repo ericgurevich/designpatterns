53ada39235e1d53803c669f3eb1bbe159c4fecd3
ericgurevich <tug00319@temple.edu>
2019-12-03 02:26:29 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import PatternBank.Bank;
+
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank(10000.00);
+    }
+}
---
+++ src/PatternBank/BankAccount.java
@@ -0,0 +1,13 @@
+package PatternBank;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+    private int id;
+    private Double balance;
+
+    public BankAccount(int id, Double initialBalance) {
+        this.id = id;
+        balance = initialBalance;
+    }
+}
